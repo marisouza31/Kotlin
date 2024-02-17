b14ba49bafc648eb2233d9d84a6ac8d6efa6c1e4
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 18:58:12 -0300

    Add files via upload

---
+++ aula6/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    val bomHumor = true
+    print("hoje estou ${if (bomHumor) "feliz" else "chateado"}")
+}
