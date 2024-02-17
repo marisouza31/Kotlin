6a89063325afa0be61292086387c13d099870997
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 20:25:46 -0300

    Add files via upload

---
+++ aula15/src/Main.kt
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamnetos.controles
+fun main(args: Array<String>) {
+    for(i in 10  downTo  1) {
+        println("i = $i")
+    }
+}
