88d25fd5a94c937e2b05c0cf04307a47f0089537
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 18:37:43 -0300

    Add files via upload

---
+++ Aula3/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamnetos
+fun main(args: Array<String>) {
+       imprimirSoma(4, 5)
+}
+fun imprimirSoma(a: Int, b: Int){
+    println(a + b)
+}
