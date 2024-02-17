c3b7893520b403a43e9b882f15d18e3bb4217770
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 19:04:40 -0300

    Add files via upload

---
+++ aula8/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamnetos
+fun main(args: Array<String>) {
+    val nota : Double = 8.5
+    if (nota >= 7.0){
+        println("aprovado")
+    }
+}
