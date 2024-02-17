642904b826e6ad8cb410fcc3644a5f01d37a6b7c
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 19:01:58 -0300

    Add files via upload

---
+++ aula7/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos
+fun main(args: Array<String>) {
+    var a: Int = 35.dec()
+    var b: String = a.toString()
+    println("Int: $a")
+    println("primeiro char string b é:  ${b.first()}")
+}
