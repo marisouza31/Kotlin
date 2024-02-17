d6c6bb0138b2c02a7614277dfd524df536761fea
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 20:19:20 -0300

    Add files via upload

---
+++ aula14/src/Main.kt
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamentos.controles
+fun main(args: Array<String>) {
+    var contador : Int = 1
+    while(contador <= 10) {
+        println(contador)
+        contador++
+    }
+}
