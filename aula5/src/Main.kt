c7fc59753cc65826ff06b9f2758e77dbbf15cfb3
Mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2024-02-17 18:54:37 -0300

    Add files via upload

---
+++ aula5/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package fundamnetos
+fun main(args: Array<String>) {
+ val aprovados = listOf("joao", "maria", "pedro")
+    print("o primeiro colocado foi ${aprovados[0]}")
+}
